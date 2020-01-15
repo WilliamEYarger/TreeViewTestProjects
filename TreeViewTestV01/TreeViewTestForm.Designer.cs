6b2dbc7c23abc6311d2422152e27b1477e378d98
WilliamEYarger <william.yarger/md@hotmail.com>
2020-01-15 09:43:51 -0500

    Add project files.

---
+++ TreeViewTestV01/TreeViewTestForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace TreeViewTestV01
+{
+    partial class TreeViewTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.addSubjectButton = new System.Windows.Forms.Button();
+            this.addDivisionNodeButton = new System.Windows.Forms.Button();
+            this.saveTreeButton = new System.Windows.Forms.Button();
+            this.loadTreeButton = new System.Windows.Forms.Button();
+            this.nodeTextValue = new System.Windows.Forms.TextBox();
+            this.getNodeNamesButton = new System.Windows.Forms.Button();
+            this.getTreeViewDictionaryButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(12, 12);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(448, 395);
+            this.treeView.TabIndex = 0;
+            // 
+            // addSubjectButton
+            // 
+            this.addSubjectButton.Location = new System.Drawing.Point(490, 114);
+            this.addSubjectButton.Name = "addSubjectButton";
+            this.addSubjectButton.Size = new System.Drawing.Size(162, 23);
+            this.addSubjectButton.TabIndex = 1;
+            this.addSubjectButton.Text = "Add Subject Node";
+            this.addSubjectButton.UseVisualStyleBackColor = true;
+            this.addSubjectButton.Click += new System.EventHandler(this.addSubjectButton_Click);
+            // 
+            // addDivisionNodeButton
+            // 
+            this.addDivisionNodeButton.Location = new System.Drawing.Point(490, 171);
+            this.addDivisionNodeButton.Name = "addDivisionNodeButton";
+            this.addDivisionNodeButton.Size = new System.Drawing.Size(162, 23);
+            this.addDivisionNodeButton.TabIndex = 2;
+            this.addDivisionNodeButton.Text = "Add Division Node";
+            this.addDivisionNodeButton.UseVisualStyleBackColor = true;
+            this.addDivisionNodeButton.Click += new System.EventHandler(this.addDivisionNodeButton_Click);
+            // 
+            // saveTreeButton
+            // 
+            this.saveTreeButton.Location = new System.Drawing.Point(490, 231);
+            this.saveTreeButton.Name = "saveTreeButton";
+            this.saveTreeButton.Size = new System.Drawing.Size(162, 23);
+            this.saveTreeButton.TabIndex = 3;
+            this.saveTreeButton.Text = "Save Tree";
+            this.saveTreeButton.UseVisualStyleBackColor = true;
+            this.saveTreeButton.Click += new System.EventHandler(this.saveTreeButton_Click);
+            // 
+            // loadTreeButton
+            // 
+            this.loadTreeButton.Location = new System.Drawing.Point(490, 296);
+            this.loadTreeButton.Name = "loadTreeButton";
+            this.loadTreeButton.Size = new System.Drawing.Size(162, 23);
+            this.loadTreeButton.TabIndex = 4;
+            this.loadTreeButton.Text = "Load Tree";
+            this.loadTreeButton.UseVisualStyleBackColor = true;
+            this.loadTreeButton.Click += new System.EventHandler(this.loadTreeButton_Click);
+            // 
+            // nodeTextValue
+            // 
+            this.nodeTextValue.Location = new System.Drawing.Point(490, 64);
+            this.nodeTextValue.Name = "nodeTextValue";
+            this.nodeTextValue.Size = new System.Drawing.Size(192, 22);
+            this.nodeTextValue.TabIndex = 5;
+            // 
+            // getNodeNamesButton
+            // 
+            this.getNodeNamesButton.Location = new System.Drawing.Point(490, 348);
+            this.getNodeNamesButton.Name = "getNodeNamesButton";
+            this.getNodeNamesButton.Size = new System.Drawing.Size(162, 23);
+            this.getNodeNamesButton.TabIndex = 6;
+            this.getNodeNamesButton.Text = "Get Node Text";
+            this.getNodeNamesButton.UseVisualStyleBackColor = true;
+            this.getNodeNamesButton.Click += new System.EventHandler(this.getNodeNamesButton_Click);
+            // 
+            // getTreeViewDictionaryButton
+            // 
+            this.getTreeViewDictionaryButton.Location = new System.Drawing.Point(490, 395);
+            this.getTreeViewDictionaryButton.Name = "getTreeViewDictionaryButton";
+            this.getTreeViewDictionaryButton.Size = new System.Drawing.Size(162, 23);
+            this.getTreeViewDictionaryButton.TabIndex = 7;
+            this.getTreeViewDictionaryButton.Text = "Get Tree View Dictioanry";
+            this.getTreeViewDictionaryButton.UseVisualStyleBackColor = true;
+            this.getTreeViewDictionaryButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TreeViewTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.getTreeViewDictionaryButton);
+            this.Controls.Add(this.getNodeNamesButton);
+            this.Controls.Add(this.nodeTextValue);
+            this.Controls.Add(this.loadTreeButton);
+            this.Controls.Add(this.saveTreeButton);
+            this.Controls.Add(this.addDivisionNodeButton);
+            this.Controls.Add(this.addSubjectButton);
+            this.Controls.Add(this.treeView);
+            this.Name = "TreeViewTestForm";
+            this.Text = "TreeViewTestForm";
+            this.Load += new System.EventHandler(this.TreeViewTestForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button addSubjectButton;
+        private System.Windows.Forms.Button addDivisionNodeButton;
+        private System.Windows.Forms.Button saveTreeButton;
+        private System.Windows.Forms.Button loadTreeButton;
+        private System.Windows.Forms.TextBox nodeTextValue;
+        private System.Windows.Forms.Button getNodeNamesButton;
+        private System.Windows.Forms.Button getTreeViewDictionaryButton;
+    }
+}
---
+++ TreeViewTestV01/TreeViewTestForm.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TreeViewTestV01
+{
+	public partial class TreeViewTestForm : Form
+	{
+
+		public TreeViewTestForm()
+		{
+			InitializeComponent();
+		}
+
+
+		private static string filePath = @"C:\CSharpProjects\TestProjects\TreeView.bin";
+		private void addSubjectButton_Click(object sender, EventArgs e)
+		{
+			treeView.Nodes.Add(nodeTextValue.Text);
+		}
+
+		private void addDivisionNodeButton_Click(object sender, EventArgs e)
+		{
+			treeView.SelectedNode.Nodes.Add(nodeTextValue.Text);
+		}
+
+		private void saveTreeButton_Click(object sender, EventArgs e)
+		{
+			if (File.Exists(filePath))
+			{
+				File.Delete(filePath);
+			}
+			// Create new array
+			ArrayList al = new ArrayList();
+			foreach (TreeNode tn in treeView.Nodes)
+			{
+				// back up every RootNode in the TreeView ...
+				al.Add(tn);
+			}
+
+			// create file
+			Stream file = File.Open(filePath, FileMode.Create);
+			// Binary formatting init.
+			BinaryFormatter bf = new BinaryFormatter();
+			try
+			{
+				// Serializing the array
+				bf.Serialize(file, al);
+			}
+			catch (System.Runtime.Serialization.SerializationException eex1)
+			{
+				MessageBox.Show("Serialization failed : {0}", eex1.Message);
+			}
+
+			// Close file
+			file.Close();
+		}
+
+		private void loadTreeButton_Click(object sender, EventArgs e)
+		{
+			if (File.Exists(filePath))
+			{
+				// open file
+				Stream file = File.Open(filePath, FileMode.Open);
+				// Binary formatting init.
+				BinaryFormatter bf = new BinaryFormatter();
+				// Object var. init.
+				object obj = null;
+				try
+				{
+					// Deserialize data from the file
+					obj = bf.Deserialize(file);
+				}
+				catch (System.Runtime.Serialization.SerializationException eex2)
+				{
+					MessageBox.Show("De-Serialization failed : {0}", eex2.Message);
+				}
+				// Close File
+				file.Close();
+
+				// Create a new array
+				ArrayList nodeList = obj as ArrayList;
+
+				// load Root-Nodes
+				foreach (TreeNode node in nodeList)
+				{
+					treeView.Nodes.Add(node);
+				}
+
+			}
+		}
+
+
+		private IEnumerable<TreeNode> Collect(TreeNodeCollection nodes)
+		{
+			foreach (TreeNode node in nodes)
+			{
+				yield return node;
+
+				foreach (var child in Collect(node.Nodes))
+					yield return child;
+			}
+		}// End IEnumerable<TreeNode> Collect
+		private void getNodeNamesButton_Click(object sender, EventArgs e)
+		{
+
+			foreach (var node in Collect(treeView.Nodes))
+			{
+				// you will see every child node here
+				string nodesText = node.Text;
+				MessageBox.Show($"The Name of this node in {nodesText}");
+				if (nodesText == "D00")
+				{
+					treeView.LabelEdit = true;
+					treeView.SelectedNode = node;
+					node.Text = "X00";
+				}
+			}// End foreach
+
+		}// End getNodeNamesButton_Click
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			Dictionary<string, string> treeViewDictionary = new Dictionary<string, string>();
+			Dictionary<string, string> inverseTreeViewDictionary = new Dictionary<string, string>();
+
+			string[] treeViewDictionaryStrings = File.ReadAllLines(@"C:\CSharpProjects\TestProjects\TreeViewDictionary.txt");
+			SortedDictionary<string, string> sortedTreeViewDictionary = new SortedDictionary<string, string>();
+			foreach (string line in treeViewDictionaryStrings)
+			{
+				string key = line.Substring(0, line.IndexOf('^'));
+				string value = line.Substring(line.IndexOf('^') + 1);
+				sortedTreeViewDictionary.Add(key, value);
+			}
+
+			foreach (KeyValuePair<string, string> kvp in sortedTreeViewDictionary)
+			{
+				//string thisLine = $"key = {kvp.Key} and value = {kvp.Value}";
+				treeViewDictionary.Add(kvp.Key, kvp.Value);
+				inverseTreeViewDictionary.Add(kvp.Value, kvp.Key);
+			}
+			treeView.BeginUpdate();
+			foreach (KeyValuePair<string,string> kvp in treeViewDictionary)
+			{
+				string nodeName = kvp.Key;
+				string nodeText = kvp.Value;
+				string[] nodeNameComponents = nodeName.Split('.');
+				string parentsName = "";
+				//for (int i=0; i< nodeNameComponents.Length-1; i++)
+				//{
+				//	parentsName = parentsName + nodeNameComponents[i] + '.';
+				//}
+				//if(parentsName.Length != 0)
+				//{
+				//	parentsName = parentsName.Substring(0, parentsName.Length - 1);
+				//}				
+				int numDots = nodeNameComponents.Length;
+				TreeNode thisNode = new TreeNode();
+				switch (numDots)
+				{
+					case 1:
+						treeView.Nodes.Add(nodeText);
+						int thisNodesNumber0 = Int32.Parse(nodeNameComponents[0]);
+						thisNode = treeView.Nodes[thisNodesNumber0];
+						thisNode.Name = nodeName;
+						break;
+					case 2:
+						int node0 = Int32.Parse(nodeNameComponents[0]);
+						treeView.Nodes[node0].Nodes.Add(nodeText);
+						thisNodesNumber0 = Int32.Parse(nodeNameComponents[0]);
+						int thisNodesNumber1 = -1;
+						if (nodeNameComponents[1].IndexOf('q') != -1)
+						{
+
+							TreeNode selectedNode = treeView.Nodes[thisNodesNumber0];
+							thisNodesNumber1 = selectedNode.GetNodeCount(true);
+
+						}
+						else
+						{
+							thisNodesNumber1 = Int32.Parse(nodeNameComponents[1]);
+						}
+						thisNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1];
+						thisNode.Name = nodeName;
+						break;
+					case 3:
+						//node0 = Int32.Parse(nodeNameComponents[0]);
+						thisNodesNumber0 = Int32.Parse(nodeNameComponents[0]);
+						thisNodesNumber1 = Int32.Parse(nodeNameComponents[1]);
+						int thisNodesNumber2 = -1;
+						if (nodeNameComponents[2].IndexOf('q') != -1)
+						{
+
+							TreeNode selectedNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1];
+							thisNodesNumber2 = selectedNode.GetNodeCount(true);
+
+						}
+						else
+						{
+							thisNodesNumber2 = Int32.Parse(nodeNameComponents[2]);
+						}
+						treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes.Add(nodeText);
+						thisNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2];
+						thisNode.Name = nodeName;
+						break;
+					case 4:
+						thisNodesNumber0 = Int32.Parse(nodeNameComponents[0]);
+						thisNodesNumber1 = Int32.Parse(nodeNameComponents[1]);
+						thisNodesNumber2 = Int32.Parse(nodeNameComponents[2]);
+						int thisNodesNumber3 = -1;
+						if (nodeNameComponents[3].IndexOf('q') != -1)
+						{
+
+							TreeNode selectedNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2];
+							thisNodesNumber3 = selectedNode.GetNodeCount(true);
+
+						}
+						else
+						{
+							thisNodesNumber3 = Int32.Parse(nodeNameComponents[3]);
+						}
+
+						treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes.Add(nodeText);
+						thisNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes[thisNodesNumber3];
+						thisNode.Name = nodeName;
+						break;
+
+					case 5:
+						thisNodesNumber0 = Int32.Parse(nodeNameComponents[0]);
+						thisNodesNumber1 = Int32.Parse(nodeNameComponents[1]);
+						thisNodesNumber2 = Int32.Parse(nodeNameComponents[2]);
+						thisNodesNumber3 = Int32.Parse(nodeNameComponents[3]);
+						int thisNodesNumber4 = -1;
+						if (nodeNameComponents[4].IndexOf('q') != -1)
+						{
+
+							TreeNode selectedNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes[thisNodesNumber3];
+							thisNodesNumber4 = selectedNode.GetNodeCount(true);
+
+						}
+						else
+						{
+							thisNodesNumber4 = Int32.Parse(nodeNameComponents[4]);
+						}
+
+						treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes[thisNodesNumber3].Nodes.Add(nodeText);
+						thisNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes[thisNodesNumber3];
+						thisNode.Name = nodeName;
+						break;
+					case 6:
+						thisNodesNumber0 = Int32.Parse(nodeNameComponents[0]);
+						thisNodesNumber1 = Int32.Parse(nodeNameComponents[1]);
+						thisNodesNumber2 = Int32.Parse(nodeNameComponents[2]);
+						thisNodesNumber3 = Int32.Parse(nodeNameComponents[3]);
+						thisNodesNumber4 = Int32.Parse(nodeNameComponents[4]);
+						int thisNodesNumber5 = -1;
+						if (nodeNameComponents[5].IndexOf('q') != -1)
+						{
+							
+							TreeNode selectedNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes[thisNodesNumber3].Nodes[thisNodesNumber4];
+							thisNodesNumber5 = selectedNode.GetNodeCount(true);
+
+						}
+						else
+						{
+							thisNodesNumber5 = Int32.Parse(nodeNameComponents[5]);
+						}						
+						treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes[thisNodesNumber3].Nodes[thisNodesNumber4].Nodes.Add(nodeText);
+						thisNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes[thisNodesNumber3].Nodes[thisNodesNumber4].Nodes[thisNodesNumber5];
+						thisNode.Name = nodeName;
+						break;
+					case 7:
+						thisNodesNumber0 = Int32.Parse(nodeNameComponents[0]);
+						thisNodesNumber1 = Int32.Parse(nodeNameComponents[1]);
+						thisNodesNumber2 = Int32.Parse(nodeNameComponents[2]);
+						thisNodesNumber3 = Int32.Parse(nodeNameComponents[3]);
+						thisNodesNumber4 = Int32.Parse(nodeNameComponents[4]);
+						thisNodesNumber5 = Int32.Parse(nodeNameComponents[5]);
+						int thisNodesNumber6 = Int32.Parse(nodeNameComponents[6]);
+						treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes[thisNodesNumber3].Nodes[thisNodesNumber4].Nodes[thisNodesNumber5].Nodes.Add(nodeText);
+						thisNode = treeView.Nodes[thisNodesNumber0].Nodes[thisNodesNumber1].Nodes[thisNodesNumber2].Nodes[thisNodesNumber3].Nodes[thisNodesNumber4].Nodes[thisNodesNumber5].Nodes[thisNodesNumber6];
+						thisNode.Name = nodeName;
+						break;
+					case 8:
+
+						break;
+					case 9:
+
+						break;
+					case 10:
+
+						break;
+					case 11:
+
+						break;
+
+
+				}
+
+			}
+			treeView.EndUpdate();
+			// Create a string[] arrayOfLines[treeViewDictionary.Count] to hold an
+			// a '^' delimited string of nodeText, nodeName,  parent'sNodeText
+			//	    string[3] arrayOfNodeValues where [0] = nodeText, [1] = nodeName, and [3] parent'sNodeText
+			//string[] arrayOfLines = new string[treeViewDictionary.Count];
+			//string delimitedNodeValues = "";
+			//int arrayOfLinesCntr = 0;
+			//foreach (KeyValuePair<string, string> kvp in treeViewDictionary)
+			//{
+			//	string nodeName = kvp.Key;
+			//	string nodeText = kvp.Value;
+			//	if (nodeName.IndexOf('.') == -1)
+			//	{
+			//		// This is a subjectNode
+			//		delimitedNodeValues = nodeText + '^' + nodeName + '^' + "";					
+			//	}
+			//	else
+			//	{
+			//		// Get parent's nodeText from nodeName
+			//		int posLastDot = nodeName.LastIndexOf('.');
+			//		string parentsNodeName = nodeName.Substring(0, posLastDot);
+			//		string parentsNodeText = treeViewDictionary[parentsNodeName];
+			//		delimitedNodeValues = nodeText + '^' + nodeName + '^' + parentsNodeText;
+			//	}
+			//	arrayOfLines[arrayOfLinesCntr] = delimitedNodeValues;
+			//	arrayOfLinesCntr++;
+			//}
+
+			// Populate the treeView from arrayOfLines
+			//treeView = new TreeView();
+			//treeView.BeginUpdate();
+			//treeView = new TreeView();
+			//for (int i=0; i< treeViewDictionary.Count; i++)
+			//{
+			//	TreeNodeCollection myNodeCollection = treeView.Nodes;
+			//	string[] nodeComponents = arrayOfLines[i].Split('^');
+			//	// Determine if this is a subject or a child node
+			//	if (nodeComponents[2] == "")
+			//	{
+			//		// This is a subject node
+			//		nodeTextValue.Text = nodeComponents[0];
+			//		addSubjectButton.PerformClick();
+
+			//		//TreeNode thisNode = new TreeNode();
+			//		//thisNode.Text = nodeComponents[0];
+			//		//thisNode.Name = nodeComponents[01];
+			//		//treeView.Nodes.Add(thisNode);
+			//		//bool index = treeView.Nodes.ContainsKey(nodeComponents[0]);
+
+			//	}
+			//	else
+			//	{
+			//		// This is a child node
+			//		TreeNode thisNode = new TreeNode();
+			//		thisNode.Text = nodeComponents[0];
+			//		thisNode.Name = nodeComponents[1];
+			//		string parentsText = nodeComponents[2];
+			//		TreeNode parentNode = new TreeNode();
+			//		parentNode.Text = parentsText;
+			//		//int index = myNodeCollection.IndexOf(parentsText);
+			//		treeView.SelectedNode = myNodeCollection.Find(parentsText, true)[0];
+			//		nodeTextValue.Text = nodeComponents[0];
+			//		addDivisionNodeButton.PerformClick();
+
+
+			//	}
+			//}
+			//treeView.EndUpdate();
+
+
+			//TreeNode node = new TreeNode();
+			//// Display a wait cursor while the TreeNodes are being created.
+			//Cursor.Current = new Cursor("MyWait.cur");
+
+			//// Suppress repainting the TreeView until all the objects have been created.
+			//treeView.BeginUpdate();
+			//foreach (KeyValuePair<string,string> kvp in treeViewDictionary)
+			//{
+			//	string nodeName = kvp.Key;
+			//	string nodeText = kvp.Value;
+			//	if (nodeName.IndexOf('.') == -1)
+			//	{
+			//		// This is a subject node
+			//		node.Text = nodeText;
+			//		node.Name = nodeName;
+			//		treeView.Nodes.Add(node);
+			//	}
+			//	else
+			//	{
+			//		// This is a divisional node
+			//		int posLastDot = nodeName.LastIndexOf('.');
+			//		string parent = nodeName.Substring(0, posLastDot);
+			//		treeView.SelectedNode = treeView.Nodes[parent];
+
+			//		node.Text = nodeText;
+			//		node.Name = nodeName;
+			//		//string parentText = inverseTreeViewDictionary[parent];
+			//		treeView.SelectedNode.Nodes.Add(nodeText);
+			//		treeView.SelectedNode = null;
+			//	}
+			//}
+			//// Reset the cursor to the default for all controls.
+			//Cursor.Current = Cursors.Default;
+
+			// Begin repainting the TreeView.
+			//treeView.EndUpdate();
+		}
+
+
+
+		//}
+
+		private void TreeViewTestForm_Load(object sender, EventArgs e)
+		{
+
+		}
+	}// End TreeViewTestForm Class
+}// End namespace GTreeViewTest01
